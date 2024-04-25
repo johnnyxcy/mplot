fbf89743c4073fd2f33c1466bb904ca75528ff83
johnnyxcy <johnny.xcy1997@outlook.com>
2024-04-25 08:17:15 +0800

    feat: bokehjs usage

--- packages/mpl/src/App.tsx
+++ packages/mpl/src/App.tsx
@@ -1,35 +1,98 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import * as React from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+import type Bokeh_ from "@bokeh/bokehjs";
+const Bokeh = window.Bokeh;
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function makeBokeh() {
+    const iid = setInterval(() => {
+        if (Bokeh !== undefined) {
+            console.log("Bokeh is loaded");
+            clearInterval(iid);
+            return;
+        } else {
+            console.log("Bokeh is not loaded");
+        }
+    }, 1000);
+    if (Bokeh === undefined) {
+        console.log("Bokeh is not loaded");
+        return null;
+    }
+    // create some data and a ColumnDataSource
+    const x = Bokeh.LinAlg.linspace(-0.5, 20.5, 10);
+    const y = x.map(function (v) {
+        return v * 0.5 + 3.0;
+    });
+    const source = new Bokeh.ColumnDataSource({ data: { x: x, y: y } });
+
+    // create some ranges for the plot
+    const xdr = new Bokeh.Range1d({ start: -0.5, end: 20.5 });
+    const ydr = new Bokeh.Range1d({ start: -0.5, end: 20.5 });
+
+    // make the plot
+    const plot = new Bokeh.Plot({
+        title: "BokehJS Plot",
+        x_range: xdr,
+        y_range: ydr,
+        width: 400,
+        height: 400,
+        background_fill_color: "#F2F2F7",
+    });
+    plot.add_tools("pan", "hover");
+
+    // add axes to the plot
+    const xaxis = new Bokeh.LinearAxis({ axis_line_color: null });
+    const yaxis = new Bokeh.LinearAxis({ axis_line_color: null });
+    plot.add_layout(xaxis, "below");
+    plot.add_layout(yaxis, "left");
+
+    // add grids to the plot
+    const xgrid = new Bokeh.Grid({ ticker: xaxis.ticker, dimension: 0 });
+    const ygrid = new Bokeh.Grid({ ticker: yaxis.ticker, dimension: 1 });
+    plot.add_layout(xgrid);
+    plot.add_layout(ygrid);
+
+    // add a Line glyph
+    const line = new Bokeh.Line({
+        x: { field: "x" },
+        y: { field: "y" },
+        line_color: "#666699",
+        line_width: 2,
+    });
+    plot.add_glyph(line, source);
+    const doc = new Bokeh.Document();
+
+    doc.add_root(plot);
+
+    const div = document.getElementById("app-plot");
+    if (div) {
+        Bokeh.embed.add_document_standalone(doc, div);
+    }
+
+    return plot;
 }
 
-export default App
+function App() {
+    const p = React.useRef<Bokeh_.Plot | null>(null);
+    React.useEffect(() => {
+        if (p.current === null) {
+            p.current = makeBokeh();
+        }
+    });
+    return (
+        <div>
+            <button
+                onClick={() => {
+                    if (p.current) {
+                        console.log(p.current);
+                    }
+                }}
+            >
+                Click Me
+            </button>
+            <div id="app-plot"></div>
+        </div>
+    );
+}
+
+export default App;
---
+++ packages/mpl/src/bokeh.d.ts
@@ -0,0 +1,3 @@
+interface Window {
+  Bokeh?: typeof import("@bokeh/bokehjs");
+}
